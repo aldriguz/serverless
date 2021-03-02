eb361f7137625eff9b7a9f0463b93eb5bdccee10
Kevin Alama <alama1992@gmail.com>
2021-03-02 01:19:51 -0500

    now I understand this crazy lambda aws better

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const emojis = ['😀','😄','😁','😆','🥳','👏','🔥','🪐','🥇'];
 //event: is the trigger event that launchs this function (https request, s3 request, etc)
 module.exports.rankly = async (event) => {
 
-  const rank = event.rawQueryString.rank; //param in query url
+  const rank = event.queryStringParameters.rank; //param in query url
   const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
 
   return {
@@ -16,10 +16,9 @@ module.exports.rankly = async (event) => {
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: rankEmoji //some reason only returns with input
-      },
-      null,
-      2
+        //input: event, //some reason only returns with input
+        emoji: rankEmoji
+      }
     ),
   };
 
