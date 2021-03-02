f595d9bc42ee8365235991d8ebc5466851ce6c6d
Kevin Alama Rodriguez <kevinalama@multiplica.com>
2021-03-02 00:42:33 -0500

    adding logic to the function and update event http

--- handler.js
+++ handler.js
@@ -1,12 +1,20 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const emojis = ['😀','😄','😁','😆','🥳','👏','🔥','🪐','🥇'];
+//lambda is stateless: not save the state of this function
+//event: is the trigger event that launchs this function (https request, s3 request, etc)
+module.exports.rankly = async (event) => {
+
+  const rank = event.rawQueryString.rank; //param in query url
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        //input: event,
+        emoji: rankEmoji
       },
       null,
       2
